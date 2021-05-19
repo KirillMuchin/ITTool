01ac40709cbdab4f37848a486901bc31c893f9b6
KirillMuchin <37753323+KirillMuchin@users.noreply.github.com>
2021-05-19 15:41:25 +0500

    Добавьте файлы проекта.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ITToolTest.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ITToolTest.Areas.Identity.IdentityHostingStartup))]
+namespace ITToolTest.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+﻿const { src, dest, watch, parallel, series } = require('gulp');
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+scss.compiler = require('node-sass');
+
+
+function cleanDist() {
+    return del('dist')
+}
+
+function styles() {
+    return src('wwwroot/sass/style.scss')
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 versions'],
+            grid: true
+        }))
+        .pipe(dest('wwwroot/css'))
+}
+
+function scripts() {
+    return src([
+        'node_modules/jquery/dist/jquery.js',
+        'wwwroot/js/site.js',
+        ])
+        .pipe(concat('site.min.js'))
+        .pipe(uglify())
+        .pipe(dest('wwwroot/js'))
+}
+
+function build() {
+    return src([
+        'wwwroot/css/style.min.css',
+        'wwwroot/fonts/**/*',
+        'wwwroot/js/site.min.js',
+        'wwwroot/*.html'
+    ], { base: 'wwwroot' })
+        .pipe(dest('dist'))
+}
+
+function images() {
+    return src('wwwroot/img/**/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(dest('wwwroot/img'))
+}
+
+function watching() {
+    watch(['wwwroot/sass/**/*.scss'], styles);
+    watch(['wwwroot/js/**/*.js', '!wwwroot/js/site.min.js'], scripts);
+}
+
+exports.styles = styles;
+exports.watching = watching;
+exports.scripts = scripts;
+exports.images = images;
+exports.cleanDist = cleanDist;
+
+exports.build = series(cleanDist, images, build);
+exports.default = parallel(styles, scripts, watching);
