e30b40fd95bee2167e926b91d74eed3daabd0bb0
KirillMuchin <37753323+KirillMuchin@users.noreply.github.com>
2021-06-04 17:41:46 +0500

    one more fix

--- Models/ChangeRoleViewModel.cs
+++ Models/ChangeRoleViewModel.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace ITToolTest.Models
+public class ChangeRoleViewModel
 {
-    public class ChangeRoleViewModel
+    public string UserId { get; set; }
+    public string UserEmail { get; set; }
+    public List<IdentityRole> AllRoles { get; set; }
+    public IList<string> UserRoles { get; set; }
+    public ChangeRoleViewModel()
     {
+        AllRoles = new List<IdentityRole>();
+        UserRoles = new List<string>();
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace ITToolTest
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
