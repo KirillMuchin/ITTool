74d5189f14631d9e5c8fdc8cf583fc7e255370ce
KirillMuchin <37753323+KirillMuchin@users.noreply.github.com>
2021-06-16 17:46:42 +0500

    main page js added for about us section

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+class TabList {
+    constructor(buttonsContainer, tabs) {
+        this.buttonsContainer = buttonsContainer;
+        this.tabs = tabs;
 
-// Write your JavaScript code.
+        this.buttonsContainer.addEventListener('click', event => {
+            const index = event.target.closest('.button').dataset.value;
+
+            this.openTab(index);
+        });
+    }
+
+    openTab(index) {
+        this.buttonsContainer.querySelector('.active-button').classList.remove('active-button');
+        this.buttonsContainer.querySelector(`.button--${index}`).classList.add('active-button');
+        this.tabs.querySelector('.active').classList.remove('active');
+        this.tabs.querySelector(`.tab--${index}`).classList.add('active');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttonsContainer = document.querySelector('.buttons');
+    const tabs = document.querySelector('.tabs');
+
+    const tabList = new TabList(buttonsContainer, tabs);
+})
